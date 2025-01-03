c5d0e76ce8f5c0c1e04c51aa8ae7f33437c91d63
CodingIran <codingiran@gmail.com>
2025-01-03 17:18:22 +0800

    Release 0.0.2

--- Sources/PrivilegedHelperKit/PrivilegedHelperKit.swift
+++ Sources/PrivilegedHelperKit/PrivilegedHelperKit.swift
@@ -5,7 +5,7 @@ import Foundation
 #error("PrivilegedHelperKit doesn't support Swift versions below 5.5.")
 #endif
 
-/// Current PrivilegedHelperKit version 0.0.1. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
-let version = "0.0.1"
+/// Current PrivilegedHelperKit version 0.0.2. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
+let version = "0.0.2"
 
 public enum PrivilegedHelperKit {}
