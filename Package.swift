48f9c67525c4c965242c50f693f8d277e93a2495
CodingIran <codingiran@gmail.com>
2025-03-11 17:41:11 +0800

    update AppleExtension

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/codingiran/AppleExtension.git", .upToNextMajor(from: "3.0.1")),
+        .package(url: "https://github.com/codingiran/AppleExtension.git", .upToNextMajor(from: "3.0.2")),
         .package(url: "https://github.com/codingiran/ScriptRunner.git", .upToNextMajor(from: "0.0.2")),
     ],
     targets: [
